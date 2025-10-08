f01d0a2c9e1f91df842c215dd288c8c1883eb61f
Vu Duc Le <54064363+vuducle@users.noreply.github.com>
2025-10-08 14:37:28 +0700

    change font

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,20 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Be_Vietnam_Pro } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from './ThemeContext';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const beVietnamPro = Be_Vietnam_Pro({
+  variable: '--font-be-vietnam-pro',
+  weight: [
+    '100',
+    '200',
+    '300',
+    '400',
+    '500',
+    '600',
+    '700',
+    '800',
+    '900',
+  ],
 });
 
 export const metadata = {
@@ -21,7 +26,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-black`}
+        className={`${beVietnamPro.variable} antialiased bg-white text-black`}
       >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
